f55e7c4df15713164ff857c127b90dcdf65aff78
zhaohaihao <zhh2933@gmail.com>
2020-02-06 17:18:49 +0800

    :sparkles: 新增设计原则-迪米特法则

---
+++ 005-LoD/src/main/java/com/zhh/Key.java
@@ -0,0 +1,18 @@
+package com.zhh;
+
+/**
+ * @author zhh
+ * @description 钥匙类
+ * @date 2020-02-06 17:10
+ */
+public class Key {
+
+    /**
+     * 转动钥匙
+     * @return
+     */
+    public boolean turns() {
+        System.out.println("转动钥匙");
+        return true;
+    }
+}
